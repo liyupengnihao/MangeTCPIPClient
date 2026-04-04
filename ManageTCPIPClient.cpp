37f306772429e64bb215f0741eb08f6769a9c752
a <a>
2026-04-04 18:02:37 +0800

    添加项目文件。

---
+++ ManageTCPIPClient.cpp
@@ -0,0 +1,21 @@
+﻿// ManageTCPIPClient.cpp : 定义静态库的函数。
+//
+
+#pragma region//头文件依赖
+#include "pch.h"
+#include "framework.h"
+
+//目前window平台下
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <Windows.h>
+#include <functional>//函数对象
+#include <vector>//动态数组
+#include <string>//字符串
+
+#pragma endregion
+
+// TODO: 这是一个库函数示例
+void fnManageTCPIPClient()
+{
+}
