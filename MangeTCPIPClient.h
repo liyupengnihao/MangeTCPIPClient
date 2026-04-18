fa942747098497cb27133d2af17722fc5f7e9d8b
a <a>
2026-04-18 20:33:34 +0800

    WPF，XAML部分修改

--- MangeTCPIPClient.h
+++ MangeTCPIPClient.h
@@ -22,10 +22,16 @@ extern "C" {//返回bool,C++为1字节，C#4字节，可以返回int，或特征
     //销毁分配的内存
     ManageTCPIPClient_API void CALL_CONV DestroyManager(void* manager);
 
-    // 客户端操作（全部使用 void* 或 char*，不暴露 std::string），0成功-1失败
+
+
+    // 哈希表中添加键clientId与对应客户端对象，0成功-1失败
     ManageTCPIPClient_API int CALL_CONV CreateClient(void* manager, const char* clientId);
-    // 断开连接并销毁，0成功，-1失败
+    // 哈希表中移除并销毁，0成功，-1失败
     ManageTCPIPClient_API int CALL_CONV DestroyClient(void* manager, const char* clientId);
+    //设置接收等待时间,0成功，-1失败,-2套接字未实例化
+    ManageTCPIPClient_API int CALL_CONV SetReceiveTimeout(void* manager, const char* clientId, int timeout);
+
+
 
     // 连接客户端，0成功，-1失败
     ManageTCPIPClient_API int CALL_CONV ConnectClient(void* manager, const char* clientId, const char* ip, int port);
@@ -34,12 +40,12 @@ extern "C" {//返回bool,C++为1字节，C#4字节，可以返回int，或特征
     // 断开连接
     ManageTCPIPClient_API void CALL_CONV DisconnectClient(void* manager, const char* clientId);
 
+
+
     // 数据收发,返回发送字节数
     ManageTCPIPClient_API int CALL_CONV SendData(void* manager, const char* clientId, const char* data, int len);
     //单次读取,返回读取的字节数
     ManageTCPIPClient_API int CALL_CONV RecvData(void* manager, const char* clientId, char* buf, int bufLen);
-    //设置接收等待时间,0成功，-1失败,-2套接字未实例化
-    ManageTCPIPClient_API int CALL_CONV SetReceiveTimeout(void* manager, const char* clientId, int timeout);
 
     // 内存释放
     //EXPORT_API void CALL_CONV FreeString(const char* str);
